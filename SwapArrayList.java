4cc52db0f47059c121e39a40e7ff15519af4f15a
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-09-06 22:12:07 +0530

    Add files via upload

---
+++ SwapArrayList.java
@@ -0,0 +1,11 @@
+class Solution{
+    public List<Integer> swapNumbers(int a, int b) {
+        List<Integer> result = new ArrayList<>();
+        result.add(b);
+        result.add(a);
+
+        return result;
+        
+    }
+    
+}
